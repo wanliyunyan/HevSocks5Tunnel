4699fb3f6ac2892c1289b7952fcf529415184fb1
潘晓升 <wanliyunyan@users.noreply.github.com>
2025-03-06 09:18:25 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HevSocks5Tunnel",
-            url:"https://github.com/wanliyunyan/HevSocks5Tunnel/releases/download/2.9.0/HevSocks5Tunnel.xcframework.zip",
-            checksum: "999142700c2abbbc048a7f6fdf081079fc9542a81365635e2b7f41ce93db65b2"
+            url:"https://github.com/wanliyunyan/HevSocks5Tunnel/releases/download/2.9.1/HevSocks5Tunnel.xcframework.zip",
+            checksum: "45ec645efc6106c5de5cf6f5c30493a5dfc88e8abd3559968553c6824e24ece8"
         )
     ]
 )
